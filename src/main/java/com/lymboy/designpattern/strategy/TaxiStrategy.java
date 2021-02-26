13434746b2b493ca8aa9b54f052e8605493de46f
lymboy <alayama@foxmail.com>
2021-02-26 21:42:18 +0800

    策略模式

--- src/main/java/com/lymboy/designpattern/strategy/TaxiStrategy.java
+++ src/main/java/com/lymboy/designpattern/strategy/TaxiStrategy.java
@@ -5,7 +5,7 @@ package com.lymboy.designpattern.strategy;
  * @version 1.0
  * @date 2021-02-26 20:10:25
  */
-public class MotoStrategy implements Strategy{
+public class TaxiStrategy implements Strategy{
 
     @Override
     public void doMethod() {
